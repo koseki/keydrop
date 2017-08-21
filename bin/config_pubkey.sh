f1e567aac3a5145d6300350e54e4d0e1703190ab
KOSEKI Kengo <koseki@gmail.com>
2017-08-22 02:12:15 +0900

    Add usage to the shellscripts

--- bin/config_pubkey.sh
+++ bin/config_pubkey.sh
@@ -4,4 +4,6 @@ set -e
 
 if [ -f "$1" ]; then
     heroku config:set PUBKEY="$(cat $1)"
+else
+    echo "Usage: config_pubkey.sh public_key_file"
 fi
--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-echo
-base64 -D $1 | openssl rsautl -decrypt -inkey $2
-echo
-
+if [ -f "$1" -a -f "$2" ]; then
+    echo
+    base64 -D $1 | openssl rsautl -decrypt -inkey $2
+    echo
+else
+    echo "Usage: decrypt.sh encrypted_file private_key_file"
+    exit 1
+fi
