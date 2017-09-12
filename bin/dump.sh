ad21be560b7d614f467f8c548d7271335c61412c
KOSEKI Kengo <koseki@gmail.com>
2017-09-13 02:27:34 +0900

    Add decrypt and dump script

---
+++ bin/dump.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+IFS=$'\t'
+
+appname=$1
+privatekey=$2
+
+if [ -z "$appname" ]; then
+    echo "Usage: dump.sh app-name [privatekey]"
+    exit 1
+fi
+
+heroku pg:psql --app $appname -c "copy keys to stdout with csv delimiter E'\t';" | while read LINE; do
+    tokens=(`echo "$LINE"`)
+    if [ ${tokens[1]} = 'publickey' -a -f "$privatekey" ]; then
+        key=$(echo ${tokens[4]} | base64 -D | openssl rsautl -decrypt -inkey $privatekey)
+        echo "${tokens[0]}$IFS${tokens[5]}$IFS${tokens[1]}$IFS${tokens[2]}$IFS${tokens[3]}$IFS$key"
+    else
+        echo "${tokens[0]}$IFS${tokens[5]}$IFS${tokens[1]}$IFS${tokens[2]}$IFS${tokens[3]}$IFS${tokens[4]}"
+    fi
+done
