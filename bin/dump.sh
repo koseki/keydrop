cc4d0013273bed4abbea02f0458a018429391d23
KOSEKI Kengo <koseki@gmail.com>
2018-09-19 18:05:59 +0900

    Avoid warning when not logged in to heroku

--- bin/dump.sh
+++ bin/dump.sh
@@ -14,10 +14,12 @@ fi
 
 heroku pg:psql --app $appname -c "copy keys to stdout with csv delimiter E'\t';" | while read LINE; do
     tokens=(`echo "$LINE"`)
-    if [ ${tokens[1]} = 'publickey' -a -f "$privatekey" ]; then
+    if [ "${tokens[1]}" = 'publickey' -a -f "$privatekey" ]; then
         key=$(echo ${tokens[4]} | base64 -D | openssl rsautl -decrypt -inkey $privatekey)
         echo "${tokens[0]}$IFS${tokens[5]}$IFS${tokens[1]}$IFS${tokens[2]}$IFS${tokens[3]}$IFS$key"
-    else
+    elif [ "${tokens[1]}" = 'htpasswd' ]; then
         echo "${tokens[0]}$IFS${tokens[5]}$IFS${tokens[1]}$IFS${tokens[2]}$IFS${tokens[3]}$IFS${tokens[4]}"
+    else
+        echo "### $LINE"
     fi
 done
